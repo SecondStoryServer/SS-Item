85fc81e1f98ec8444ad4466f729d997111ddbe07
sya-ri <sya79lua@gmail.com>
2020-05-12 00:01:10 +0900

    launchLaserの処理を書いた 攻撃が入ることを確認

--- src/main/kotlin/me/syari/ss/item/equip/weapon/indirect/WandItem.kt
+++ src/main/kotlin/me/syari/ss/item/equip/weapon/indirect/WandItem.kt
@@ -1,10 +1,10 @@
 package me.syari.ss.item.equip.weapon.indirect
 
+import me.syari.ss.battle.damage.DamageCalculator
 import me.syari.ss.battle.equipment.ElementType
+import me.syari.ss.battle.status.EntityStatus
 import me.syari.ss.core.item.CustomItemStack
 import me.syari.ss.core.particle.CustomParticle
-import me.syari.ss.core.scheduler.CustomScheduler.runTimer
-import me.syari.ss.item.Main.Companion.itemPlugin
 import me.syari.ss.item.custom.ClickableItem
 import me.syari.ss.item.custom.ItemType
 import me.syari.ss.item.equip.weapon.EnhancedWeaponItem.Companion.getAttackSpeedCoolDownTick
@@ -12,7 +12,6 @@ import me.syari.ss.item.equip.weapon.WeaponItem
 import me.syari.ss.item.equip.weapon.WeaponType
 import org.bukkit.Material
 import org.bukkit.entity.Player
-import org.bukkit.entity.Snowball
 
 class WandItem(
     override val id: String,
@@ -30,20 +29,38 @@ class WandItem(
 
     override fun onClick(player: Player, item: CustomItemStack, clickType: ClickableItem.ClickType): Boolean {
         if (clickType.isRight) {
-            // TODO 雪玉ではなくレーザーに変更
+            launchLaser(player, item)
             return true
         }
         return false
     }
 
-    private fun applyParticle(snowball: Snowball) {
+    private fun launchLaser(player: Player, item: CustomItemStack) {
         val particle = CustomParticle.RedStone(155, 89, 182, 10, 0.0)
-        runTimer(itemPlugin, 10) {
-            if (snowball.isValid) {
-                cancel()
-            } else {
-                particle.spawn(snowball)
+        val maxDistance = 10
+        val interDistance = 0.5
+        val beginLocation = player.location
+        val direction = beginLocation.direction.normalize()
+        var t = 0.0
+        while (t <= maxDistance) {
+            t += interDistance
+            val x = direction.x * t
+            val y = direction.y * t + 1.5
+            val z = direction.z * t
+            val interLocation = beginLocation.clone().add(x, y, z)
+            if (!interLocation.block.isPassable) {
+                break
             }
+            val nearEntity = interLocation.getNearbyLivingEntities(interDistance)
+            if (nearEntity.isNotEmpty()) {
+                val nearestEntity = nearEntity.first()
+                val victimStatus = EntityStatus.from(nearestEntity)
+                val enhancedItem = getEnhanced(item)
+                val attackerStatus = enhancedItem.getAttackStatus(player)
+                val damage = DamageCalculator.getDamage(attackerStatus, victimStatus)
+                nearestEntity.damage(damage.toDouble(), player)
+            }
+            particle.spawn(interLocation)
         }
     }
 }
